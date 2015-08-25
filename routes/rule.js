b55a620ff33d7b45b4122fa296f58f4d4ba1a266
nick <ncksllvn@gmail.com>
2015-08-24 22:21:49 -0400

    Add rule route; basic boilerplate

---
+++ routes/rule.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+/* GET random rule */
+router.get('/', (req, res, next) => {
+  
+  var rule = {
+    id:           123456,
+    name:         'The Little Man',
+    image:        'http://placehold.it/400x400',
+    permalink:    req.protocol + '://' + req.hostname + '/rule/' + 123456,
+    description: `Before you take a drink, remove the (invisible) little man from your cup. 
+                  Afterwards, put him back on. Forget either and continue taking drinks until 
+                  you remember.`
+  }
+  
+  rule.title = rule.name
+  
+  res.render('rule', rule)
+})
+
+module.exports = router
