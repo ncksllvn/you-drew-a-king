5da94a10cd7dfb1116fa2cc3e2c979cbe65c1022
nick <ncksllvn@gmail.com>
2015-08-30 23:00:45 -0400

    Add colors and general style

--- routes/rule.js
+++ routes/rule.js
@@ -9,7 +9,7 @@ router.get('/', (req, res, next) => {
   var rule = {
     id:           123456,
     name:         'The Little Man',
-    image:        'http://placehold.it/400x400',
+    image:        'http://placehold.it/200x200',
     permalink:    req.protocol + '://' + req.hostname + '/rule/' + 123456,
     description: `Before you take a drink, remove the (invisible) little man from your cup. 
                   Afterwards, put him back on. Forget either and continue taking drinks until 
