d9eef578c978147782586c6039becc2c5bfdae62
nick sullivan <ncksllvn@gmail.com>
2015-09-16 17:25:03 -0400

    Add how to play

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'King\'s Cup Rule Master' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'King\'s Cup Rule Master' })
 });
 
-module.exports = router;
+router.get('/how-to-play', (req, res, next) => {
+  res.render('how-to-play')
+})
+
+module.exports = router
