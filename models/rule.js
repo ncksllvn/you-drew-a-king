559a06100d3a139e006a02f5b388fd782f673491
nick sullivan <ncksllvn@gmail.com>
2015-09-17 17:30:37 -0400

    Using CLI tool to create models

--- models/rule.js
+++ models/rule.js
@@ -1,32 +1,14 @@
-module.exports = (sequelize, Datatypes) => {
-	
-	var Rule = sequelize.define('rule', {
-		
-		title: {
-			type: Datatypes.STRING
-		},
-		
-		description: {
-			type: Datatypes.TEXT
-		}
-		
-	}, {
-		classMethods: {
-			
-			associate: function(models){
-				Rule.hasMany(models.like)
-			},
-			
-			random: function(){
-				return {
-					where: {
-						
-					}
-				}
-			}
-			
-		}
-	})
-	
-	return Rule
-}
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Rule = sequelize.define('Rule', {
+    title: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Rule;
+};
