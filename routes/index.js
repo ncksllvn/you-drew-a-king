dec82161dc221699692577645d7c886af874c54a
nick sullivan <ncksllvn@gmail.com>
2015-09-17 17:24:27 -0400

    Change titles

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'King\'s Cup Rule Master' })
+  res.render('index', { title: 'You Drew A King.' })
 });
 
 router.get('/how-to-play', (req, res, next) => {
-  res.render('how-to-play')
+  res.render('how-to-play', { title: 'How to play' })
 })
 
 module.exports = router
