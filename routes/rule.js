b89c6fe33d03b5ad21e5a4a668eb9eced0712292
nick <ncksllvn@gmail.com>
2015-09-18 12:43:07 -0400

    Change name to title

--- routes/rule.js
+++ routes/rule.js
@@ -5,19 +5,14 @@ var router = express.Router();
 /* GET random rule */
 router.get('/', (req, res, next) => {
   
-  var rule = {
-    id:           123456,
-    name:         'The Little Man',
-    //image:        'http://placehold.it/200x200',
-    permalink:    req.protocol + '://' + req.hostname + '/rule/' + 123456,
-    description: `Before you take a drink, remove the (invisible) little man from your cup. 
-                  Afterwards, put him back on. Forget either and continue taking drinks until 
-                  you remember.`
-  }
+  Rule.findAll().then((rules) => {
+    
+    var rule = rules[0]
+    
+    res.render('rule', rule.get({ plain: true }))
+    
+  })
   
-  rule.title = rule.name
-  
-  res.render('rule', rule)
 })
 
 module.exports = router
