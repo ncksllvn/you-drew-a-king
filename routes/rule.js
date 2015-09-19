549431caf09615fae49bf50eff2174a473230aa3
nick <ncksllvn@gmail.com>
2015-09-19 11:32:53 -0400

    Add route for rules by id

--- routes/rule.js
+++ routes/rule.js
@@ -1,14 +1,38 @@
+var express = require('express')
+var slug = require('slug')
+var router = express.Router()
 var { Rule } = require('../models')
-var express = require('express');
-var router = express.Router();
+
+var renderRule = (req, res, rule) => {
+  
+  rule = rule.get({ plain: true })
+  
+  var ruleId = rule.id
+  var ruleTitle = slug(rule.title)
+  var permalink = req.protocol + '://' + req.get('host') + '/rule/' + ruleTitle + '/' + ruleId
+  var ruleWithPermalink = Object.assign({
+    permalink: permalink
+  }, rule)
+  
+  res.render('rule', ruleWithPermalink)
+  
+}
 
 /* GET random rule */
 router.get('/', (req, res, next) => {
   
   Rule.findRandom().then((rule) => {
-    
-    res.render('rule', rule.get({ plain: true }))
-    
+    renderRule(req, res, rule)
+  })
+  
+})
+
+router.get('/:title/:id', (req, res, next) => {
+  
+  var ruleId = req.params.id
+  
+  Rule.findById(ruleId).then((rule) => {
+    renderRule(req, res, rule)
   })
   
 })
