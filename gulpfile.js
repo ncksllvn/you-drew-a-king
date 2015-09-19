1d36f04fe92e4288a69ee9d1ff0eced7e10c1b78
nick <ncksllvn@gmail.com>
2015-09-19 15:14:49 -0400

    Add js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp')
+var concat = require('gulp-concat')
 var concatCss = require('gulp-concat-css')
 var cssnext = require("gulp-cssnext")
 
 gulp.task('default', function(){
 
+})
+
+gulp.task('js', function(){
+	return gulp.src([
+			'public/javascripts/*.js'
+		], { base: ''})
+		.pipe(concat('js/bundle.js'))
+		.pipe(gulp.dest('public/build/'))
 })
 
 gulp.task('css', function(){
