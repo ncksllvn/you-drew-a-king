64631933a1a88e34221e643c3d6108c19e32c53e
nick <ncksllvn@gmail.com>
2015-09-20 19:32:22 -0400

    Add logic for sharing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,37 @@
 (function(){
-	var shareBtn = document.getElementById('share-button')
+	
+	var toggleShareOptionsBtn = document.getElementById('share-button')
 	var shareOptions = document.getElementById('share-options')
 	
-	shareBtn.onclick = function(){
-		toggleClass(shareOptions, 'in')
+	if (toggleShareOptionsBtn)
+	{
+		toggleShareOptionsBtn.onclick = function(){
+			toggleClass(shareOptions, 'in')
+		}
+	}
+	
+	var permalinkInput = document.getElementById('permalink')
+	
+	if (permalinkInput)
+	{
+		permalinkInput.onclick = function(){
+			this.setSelectionRange(0, this.value.length)
+		}
+	}
+	
+	var shareLinks = document.getElementsByClassName('share-link')
+	var openShareWindow = function(event){
+		var href = this.getAttribute('href')
+		
+		openPopup(href, null, 600, 325)
+		event.preventDefault()
+		
+		return false
+	}
+	
+	for (var i = 0; i < shareLinks.length; i++)
+	{
+		shareLinks[i].onclick = openShareWindow
 	}
 
 })();
---
+++ public/javascripts/open-popup.js
@@ -0,0 +1,8 @@
+function openPopup(url, title, w, h) {
+    var wLeft = window.screenLeft ? window.screenLeft : window.screenX;
+    var wTop = window.screenTop ? window.screenTop : window.screenY;
+
+    var left = wLeft + (window.innerWidth / 2) - (w / 2);
+    var top = wTop + (window.innerHeight / 2) - (h / 2);
+    return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+}
