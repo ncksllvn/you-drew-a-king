62d1617d51df64ce2003f5fccc01962ca64a140a
nick <ncksllvn@gmail.com>
2015-09-28 15:27:40 -0400

    Add opensearch

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,9 @@ router.get('/', (req, res, next) => {
   res.render('index')
 });
 
-var howToPlay = {
+router.get('/how-to-play', (req, res, next) => res.render('how-to-play', {
   title: 'How to Play',
   description: 'The classic method of how to play Kings.'
-}
-
-router.get('/how-to-play', (req, res, next) => {
-  res.render('how-to-play', howToPlay)
-})
+}))
 
 module.exports = router
