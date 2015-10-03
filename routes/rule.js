3062b4934da7a31ae0994351dbbd66dff68e3d1d
nick <ncksllvn@gmail.com>
2015-10-03 14:55:33 -0400

    Implement algorithm to prevent duplicate rules

--- routes/rule.js
+++ routes/rule.js
@@ -2,18 +2,22 @@ var express = require('express')
 var slug = require('slug')
 var router = express.Router()
 var { Rule } = require('../models')
-
+var prevRulesMaxLength = 55
 
 router.get('/rule', (req, res, next) => {
   
-  var lastRuleId = req.query.last
-  var whereClause = lastRuleId && {
-    id: {
-      $ne: lastRuleId  
-    }
-  }
+  var prevRules = req.session.prevRules = req.session.prevRules || []
   
-  Rule.findRandom(whereClause).then((rule) => {
+  Rule.findRandom({
+    id: {
+      $notIn: prevRules.length > 0 ? prevRules : [-1]
+    }
+  }).then((rule) => {
+    
+    prevRules.push(rule.id)
+    
+    if (prevRules.length > prevRulesMaxLength)
+      req.session.prevRules = prevRules.slice(1)
     
     res.render('rule', {
       title: 'Get a Rule',
