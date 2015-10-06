47b83e9d4847573939f2f76515159e086fb43115
nick <ncksllvn@gmail.com>
2015-10-06 17:34:22 -0400

    Add a sitemap

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var { Rule } = require('../models')
+var moment = require('moment')
 var router = express.Router()
 
 router.get('/', (req, res, next) => {
@@ -9,5 +11,16 @@ router.get('/how-to-play', (req, res, next) => res.render('how-to-play', {
   title: 'How to Play',
   description: 'The classic method of how to play Kings.'
 }))
+
+router.get('/sitemap.xml', (req, res, next) => {
+  
+  Rule.findAll().then((rules)=>{
+    
+    res.type('xml')
+    res.render('sitemap', { rules: rules, moment: moment })
+    
+  })
+  
+})
 
 module.exports = router
