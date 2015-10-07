4c6a80e276810603717b1096f01b1a93f4d26396
nick <ncksllvn@gmail.com>
2015-10-07 11:28:46 -0400

    Default to development

--- constants/locals.js
+++ constants/locals.js
@@ -6,27 +6,27 @@ module.exports = ()=>{
 		siteDescription: 'All of the best rules to play during Kings, King\'s Cup and the Ring of Fire.',
 		facebookAppId: (function(){
 			switch(process.env.NODE_ENV){
-				case 'development':
-					return '181164828884864'
 				case 'staging':
 					return '182594082075272'
 				case 'production':
-				default:
 					return '177455305922483'
+				case 'development':
+				default:
+					return '181164828884864'
 			}
 		})(),
 		googleAnalyticsId: 'UA-67867718-1',
 		host: (function(){
 			switch (process.env.NODE_ENV){
-				case 'development':
-					return 'http://localhost:3000'
 				case 'staging':
 					return 'http://sta.youdrewaking.com'
 				case 'production':
-				default:
 					return 'http://www.youdrewaking.com'
+				case 'development':
+				default:
+					return 'http://localhost:3000'
 			}
 		})(),
-		buildDirectory: process.env.NODE_ENV == 'development' ? '/build/' : '/dist/'
+		buildDirectory: process.env.NODE_ENV && 'staging,production'.indexOf(process.env.NODE_ENV) >= 0 ? '/dist/' : '/build/'
 	}
 }
