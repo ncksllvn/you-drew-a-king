da41da350b13017343d65bdb7b3aae408b13f26d
nick <ncksllvn@gmail.com>
2016-01-17 17:52:23 -0500

    Add the Wave

---
+++ seeders/20160117174536-unnamed-seeder.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var Rule = require('../models').Rule;
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    
+    return Rule.create({
+      
+      title: 'The Wave',
+      
+      description: 'At any point during the game, start <a href="https://en.wikipedia.org/wiki/Wave_(audience)">The Wave</a>. Any player that joins late or fails to join altogether must drink.',
+      
+      submitter: 'Zoey',
+      
+      image: 'The-Wave.png'
+      
+    })
+    
+  },
+
+  down: function (queryInterface, Sequelize) {
+    
+    return Rule.destroy({
+      where: {
+        title: 'The Wave'
+      }
+    })
+    
+  }
+};
